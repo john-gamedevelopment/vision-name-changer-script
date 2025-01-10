548470e74c6a662625344650d536267479d7ff20
john <hugeaxel043@gmail.com>
2025-01-09 21:32:43 -0500

    Update vision name changin script.lua

--- vision name changin script.lua
+++ vision name changin script.lua
@@ -1,6 +1,6 @@
 local args = {
-    [1] = "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-    [2] = "Male"
+    [1] = "placeholder", -- put the name u want here
+    [2] = "Male" -- put the gender you want here, either "Male" or "Female"
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SendTime"):FireServer(unpack(args))
